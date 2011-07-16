3b1f0f05dee2eca22f2fefda9ac539b0c28cb485
Carson Tang <tang.carson@gmail.com>
2011-07-16 15:58:23 -0700

    Learned about loops

--- intro.clj
+++ intro.clj
@@ -1,20 +1,18 @@
+; Syntactic sugar for (def (fn ...) )
 (defn print-down-from [x]
 	(when (pos? x)
 		(println x)
 		(recur (dec x))))
 
+; Tail recursion
 (defn sum-from-down [sum x]
 	(if (pos? x)
 		(recur (+ sum x) (dec x))
 		sum))
 
-(defn even [x]
-	(let [remainder (mod x 2)]
-		(if (= remainder 0) true false)))
-
 (defn sum-evens-helper [sum x]
 	(let [is-positive (pos? x)
-			is-even (even x)]
+			is-even (even? x)]
 			;(do (println "Current number: " x) (println "Even?: " is-even) (println "Positive?: " is-positive))
 			(if is-positive
 				(if is-even
@@ -24,3 +22,20 @@
 				
 (defn sum-evens [x]
 	(sum-evens-helper 0 x))
+	
+; Loop, tail recursion w/o the use of a helper function, NEAT!
+(defn sum-down-from [initial-x]
+	(loop [sum 0, x initial-x]
+		(if (pos? x)
+			(recur (+ sum x) (dec x))
+			sum)))
+
+(defn idiomatic-sum-evens [initial-x]
+	(loop [sum 0, x initial-x]
+		(let [is-positive (pos? x)
+			   is-even (even? x)]
+			(if is-positive
+				(if is-even
+					(recur (+ sum x) (dec x))
+					(recur sum (dec x)))
+				sum))))
